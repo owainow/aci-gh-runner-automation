82e8a3aa50caca54e001ae16f4c55a9ad6cb6558
owainow <owainosborne@tigerbay.uk.com>
2023-05-18 12:50:32 +0100

    Added variables to ACI module

---
+++ Terraform/modules/aci/variables.tf
@@ -0,0 +1,21 @@
+variable location {
+  type=string
+  default="EastUs"
+} 
+variable resourceGroupName {
+  type=string
+  default="oow-aks-zero-trust-demo"
+} 
+
+variable vnetName {
+    type = string
+    default = "vnet-oow-zerotrust"
+}
+
+variable gh_pat {
+    default = ""
+}
+
+variable gh_repo_url {
+    default = ""
+}

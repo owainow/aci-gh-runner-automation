063ab58dbdba8f253f2efcc7882caf8939deff3d
owainow <owainosborne@tigerbay.uk.com>
2023-05-21 20:02:23 +0100

    Added depends on

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -32,6 +32,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 resource "azurerm_virtual_network" "vnet" {
+  depends_on = [ azurerm_resource_group.rg ]
   name                = var.vnet_name
   address_space       = ["10.240.10.0/24"]
   resource_group_name = var.rg_name
